c0bffea681c73a23a5a414eb64d376eaa867e3bc
Stiven9927 <estelarmaximo@gmail.com>
2022-01-26 19:15:42 -0500

    2do commit 19:15

--- app/src/main/java/com/morales/apps/main_nuevo_msmm/MainActivity.java
+++ app/src/main/java/com/morales/apps/main_nuevo_msmm/MainActivity.java
@@ -2,9 +2,12 @@ package com.morales.apps.main_nuevo_msmm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText user;
+    EditText pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
